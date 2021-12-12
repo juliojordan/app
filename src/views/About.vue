dd8bd3bcb7cae4ee3bcd0dea5ed84a5cd6ddeb86
Julio Jordán <juliojordan@protonmail.com>
2021-12-12 10:51:31 +0100

    add tabs

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Out and about</h1>
+    <ul>
+      <li>
+        <a href="#dash">Dash</a>
+      </li>
+      <li>
+        <a href="#config">Config</a>
+      </li>
+    </ul>
+    <h2 id="dash">Dash</h2>
+    <h2 id="config">Config</h2>
   </div>
 </template>
+<style scoped>
+ul {
+  list-style-type: none;
+}
+</style>
